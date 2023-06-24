a5aa848b70f760c1423cd239317b04161b18cf1e
Dietmar Schoder <dietmar@schoder.uk>
2023-06-24 10:40:26 +0100

    Improved error message

--- ChainExample/Helpers/SlackManager.cs
+++ ChainExample/Helpers/SlackManager.cs
@@ -8,12 +8,16 @@ namespace ChainExample.Helpers
     {
         public SlackManager() { }
 
-        public async Task SlackErrorAsync(string messageBody)
+        public async Task SlackErrorChainResultAsync(ChainResult chainResult)
             => await new SbmClient(SlackSecrets.SLACK_WEBHOOKURL_ERROR).Send(
                 new Message
                 {
                     Username = SlackSecrets.SLACK_USER,
-                    Text = messageBody,
+                    Text = $"{DateTime.UtcNow} {chainResult.CalledBy}" +
+                        $"\r\n--------------------" +
+                        $"\r\n{string.Join("\r\n", chainResult.StackTrace)}" +
+                        $"\r\n--------------------" +
+                        $"\r\n{chainResult.Exception.ToString()}",
                     IconEmoji = Emoji.Bomb
                 });
     }
