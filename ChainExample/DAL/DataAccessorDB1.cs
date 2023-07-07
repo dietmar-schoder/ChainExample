b17ea335fd56279defa4e21d17033655ceb0e081
Dietmar Schoder <dietmar@schoder.uk>
2023-07-07 11:02:53 +0100

    Test SonarLint

--- ChainExample/DAL/DataAccessorDB1.cs
+++ ChainExample/DAL/DataAccessorDB1.cs
@@ -4,6 +4,7 @@ namespace ChainExample.DAL
 {
     public class DataAccessorDB1 : IDataAccessorDB1
     {
-        public Task<bool> UpdateAsync(AnyDataObject anyDataObject) => Task.FromResult(true);
+        public Task<bool> UpdateAsync(AnyDataObject anyDataObject)
+            => Task.FromResult(true);
     }
 }
